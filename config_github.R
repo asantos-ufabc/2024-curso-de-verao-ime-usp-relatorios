92a6e270caca058d371636e0f197ccaf4c384766
Alan Santos <a.santos@aluno.ufabc.edu.br>
2024-05-10 19:30:32 -0300

    criação do script

--- config_github.R
+++ config_github.R
@@ -8,11 +8,15 @@ p_load(tidyverse, here, skimr, usethis)
 usethis::use_git()
 
 
+# GIT CONFIG --------------------------------------------------------------
+usethis::use_git_remote("origin", url = NULL, overwrite = TRUE)
+
 # GITHUB ------------------------------------------------------------------
 usethis::use_github(
   private = FALSE
   #organisation = ""
 )
+
 
 
 # README ------------------------------------------------------------------
